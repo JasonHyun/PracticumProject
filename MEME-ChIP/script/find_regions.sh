d07c15d4d2eeafd14af7c3d4c6de4de32cfb7010
jqulr <junequ99@gmail.com>
2025-04-17 03:35:47 -0400

    pancreas species added

--- MEME-ChIP/script/find_regions.sh
+++ MEME-ChIP/script/find_regions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=bedtools_test
+#SBATCH --job-name=find_seq
 #SBATCH -p RM-shared
 #SBATCH -t 4:00:00
 #SBATCH -n 16
@@ -8,24 +8,41 @@
 #SBATCH --mail-type=ALL
 
 #SBATCH --mail-user=juneq@andrew.cmu.edu
-#SBATCH --output slurm_out/bedtools_test.out
-#SBATCH --error slurm_out/bedtools_test.err
+#SBATCH --output slurm_out/find_seq.out
+#SBATCH --error slurm_out/find_seq.err
 
 module load bedtools
 
-cd /ocean/projects/bio230007p/jqu1/bedtools_closest
+cd /ocean/projects/bio230007p/jqu1/human_genome
 
-input="/ocean/projects/bio230007p/jqu1/bedtools_subset/results/species_liver.bed"
-TSS="/ocean/projects/bio230007p/jqu1/human_genome/gencode.v27.annotation.protTranscript.TSSsWithStrand_sorted.bed"
+# get enhancer/promoter sequences (mapped to human genome)
+genome="/ocean/projects/bio230007p/jqu1/human_genome/hg38.fa"
+regions="/ocean/projects/bio230007p/jqu1/bedtools_closest/pancreas_species_regions/pancreas_species_promoters_1kb.bed"
+output1="/ocean/projects/bio230007p/jqu1/bedtools_closest/pancreas_species_seqs/pancreas_species_promoters_seq.fa"
+output2="/ocean/projects/bio230007p/jqu1/bedtools_closest/pancreas_species_seqs/pancreas_species_promoters_uniq.fa"
 
-# promoters (within 1kb)
-bedtools window -a $input -b $TSS -w 1000 > liver_species_promoters_1kb.bed
+bedtools getfasta -fi $genome \
+ -bed $regions -fo $output1
 
-# enhancers (within 5kb-200kb)
+ awk '
+  /^>/ {
+    if (++count[$0] == 1) {
+      header = $0;
+      getline seq;
+      seqs[header] = seq;
+    }
+  }
+  END {
+    for (h in count) {
+      if (count[h] == 1) {
+        print h "\n" seqs[h];
+      }
+    }
+  }
+' $output1 > $output2
 
-bedtools closest -a $input -b $TSS -D a > liver_species_tss_distance.bed
 
-# filter out regions between 5kb-200kb upstream and downstream (likely enhancers)
-awk '($NF <= -5000 && $NF >= -200000) || ($NF >= 5000 && $NF <= 200000)' liver_species_tss_distance.bed > liver_species_enhancers_5kb_200kb.bed
+
+
 
 
