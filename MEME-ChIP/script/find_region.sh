69a02726df49620e1887dcb95d3096cb9a46b1b6
jqulr <junequ99@gmail.com>
2025-04-27 09:22:26 -0400

    percent conservation added

--- MEME-ChIP/script/find_region.sh
+++ MEME-ChIP/script/find_region.sh
@@ -6,24 +6,31 @@
 #SBATCH -n 16
 #SBATCH --account bio230007p
 #SBATCH --mail-type=ALL
-
 #SBATCH --mail-user=juneq@andrew.cmu.edu
 #SBATCH --output slurm_out/bedtools_test.out
 #SBATCH --error slurm_out/bedtools_test.err
 
 module load bedtools
 
-cd /ocean/projects/bio230007p/jqu1/bedtools_closest/pancreas_species_regions
+cd /Users/junequ/PracticumProject/regulatory_elements/mouse_full_peak
 
-input="/ocean/projects/bio230007p/jqu1/bedtools_subset/results/species_pancreas_parsed.bed"
-TSS="/ocean/projects/bio230007p/jqu1/human_genome/gencode.v27.annotation.protTranscript.TSSsWithStrand_sorted.bed"
+# input: .gz file (or .bed file directly) of genomic regions to separate out enhancer and promoters
+input1="/ocean/projects/bio230007p/jqu1/MouseAtac/Pancreas/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.gz"
+input="/ocean/projects/bio230007p/jqu1/MouseAtac/Pancreas/peak/idr_reproducibility/idr.conservative_peak.narrowPeak.bed"
 
-# promoters (within 1kb)
-bedtools window -a $input -b $TSS -w 1000 > pancreas_species_promoters_1kb.bed
+# Decompress file
+gunzip -c "$input1" > "$input"
 
-# enhancers (within 5kb-200kb)
+TSS="/ocean/projects/bio230007p/jqu1/mouse_genome/gencode.vM15.annotation.protTranscript.geneNames_TSSWithStrand_sorted.bed"
 
-bedtools closest -a $input -b $TSS -D a > pancreas_species_tss_distance.bed
+sortedInput="mouse_pancreas_full_peak_unique_sorted.bed"
+bedtools sort -i "$input" > "$sortedInput"
 
-# filter out regions between 5kb-200kb upstream and downstream (likely enhancers)
-awk '($NF <= -5000 && $NF >= -200000) || ($NF >= 5000 && $NF <= 200000)' pancreas_species_tss_distance.bed > pancreas_species_enhancers_5kb_200kb.bed
+# Promoters (within 1kb)
+bedtools window -a "$sortedInput" -b "$TSS" -w 1000 > mouse_pancreas_full_peak_promoters_1kb.bed
+
+# Enhancers (within 5kb-200kb)
+bedtools closest -a "$sortedInput" -b "$TSS" -D a > mouse_pancreas_tss_distance.bed
+
+awk '($NF <= -5000 && $NF >= -200000) || ($NF >= 5000 && $NF <= 200000)' \
+  mouse_pancreas_tss_distance.bed > mouse_pancreas_full_peak_enhancers_5kb_200kb.bed
