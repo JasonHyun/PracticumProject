7651122c2022e2c8c10c2c7039f8152bbd091b70
DeyuanXu <xudeyuan1998@163.com>
2025-04-27 20:43:40 -0400

    Add files via upload

---
+++ Pipeline_V2.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+###########################################################
+# This is a pipeline to run all code-based tasks for our  #
+# final project. Note that this pipeline may take more    #
+# than 24 hrs to finish. Use command "squeue -u [usrname] #
+# to check the status of the tasks running on PSC.        #
+# Members: Deyuan Xu, June Qu, Jason Hyun, Jessica Vu     #
+# *Make sure you have access permission to all 4 members' #
+# home directory before running this pipeline             #
+# Tissues studied: Liver & Pancreas                       #
+# Species involved: Mouse & Human                         #
+###########################################################
+
+###########################################################
+#Usage: put this script in the same directory where all the
+#peak files are placed. Run this pipeline. A HALPER must
+#be installed in your environment. Please use the first
+#parameter to give a path to mouse liver peak file and the 
+#second parameter to give a path to mouse pancreas peak
+#file. Besides, use the third parameter to give a path to
+#the human liver peak file and the fourth parameter to 
+#give a human pancreas peak file.
+###########################################################
+
+mouse_liver_peak=$1
+mouse_pancreas_peak=$2
+human_liver_peak=$3
+human_liver_peak=$4
+
+
+
+###########################################################
+# Map mouse liver OCRs to human genome
+###########################################################
+
+job_filename="liver.job"
+
+cat > "$job_filename" <<EOL
+#!/bin/bash
+#SBATCH -p RM-shared
+#SBATCH -t 24:00:00
+#SBATCH --mem 1999M
+#SBATCH -n 16
+#SBATCH --account bio230007p
+#SBATCH -J liver_job
+#SBATCH -e ./liver_pipeline.err
+#SBATCH -o ./liver_pipeline.out
+#SBATCH --export=ALL
+
+#echo commands to stdout
+set -x
+
+bash halper_map_peak_orthologs.sh \\
+-b ./$1 \\
+-o ./mapping_mouse2human/liver \\
+-s Mouse \\
+-t Human \\
+-c /ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal
+EOL
+
+echo "job file created: $job_filename"
+sbtach ./liver.job
+rm ./liver.job
+
+###########################################################
+# Map mouse pancreas OCRs to human genome
+###########################################################
+
+job_filename="pancreas.job"
+
+cat > "$job_filename" <<EOL
+#!/bin/bash
+#SBATCH -p RM-shared
+#SBATCH -t 24:00:00
+#SBATCH --mem 1999M
+#SBATCH -n 16
+#SBATCH --account bio230007p
+#SBATCH -J pancreas_job
+#SBATCH -e ./pancreas_pipeline.err
+#SBATCH -o ./pancreas_pipeline.out
+#SBATCH --export=ALL
+
+#echo commands to stdout
+set -x
+
+bash halper_map_peak_orthologs.sh \\
+-b ./$2 \\
+-o ./mapping_mouse2human/pancreas \\
+-s Mouse \\
+-t Human \\
+-c /ocean/projects/bio230007p/ikaplow/Alignments/10plusway-master.hal
+EOL
+
+echo "job file created: $job_filename"
+sbtach ./pancreas.job
+rm ./pancreas.job
+
+###########################################################
+# Wait until the HALPER is done
+###########################################################
+
+# sleep for 5 hours before checking status because HALPER definetely needs
+# more than 5 hours
+
+echo "waiting for mapping to be done..."
+sleep 18000
+
+username="dxua"
+
+# check task status every 10 minutes
+while true; do
+    
+    job_count=$(squeue -u "$username" | wc -l)
+
+   
+    job_count=$((job_count - 1))
+
+    if [ "$job_count" -eq 0 ]; then
+        echo "PSC task is done! Proceed on next steps."
+        break
+    else
+        echo "PSC task is not done, next check in 10 minutes..."
+        sleep 600  
+    fi
+done
+
+###########################################################
+# Extract unique and conservative peaks across species or across tissues
+###########################################################
+
+bash /ocean/projects/bio230007p/dxua/code/intersection.sh
+
+
+###########################################################
+# Find enhancer and promoter regions
+###########################################################
+
+bash /ocean/projects/bio230007p/dxua/code/find_region.sh
+
+
+###########################################################
+# Get sequences of OCRs for MEME
+###########################################################
+
+bash /ocean/projects/bio230007p/dxua/code/region2seq.sh
+
+
+###########################################################
+# Calculate the percentatages of conserved OCRs
+###########################################################
+
+bash /ocean/projects/bio230007p/dxua/code/find_percent_conservation.sh
+
+###########################################################
+# Usage of MEME for finding motifs
+###########################################################
+
+bash /ocean/projects/bio230007p/dxua/code/meme_chip.sh
+
+
+
